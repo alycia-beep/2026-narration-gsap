51b1cbbf077f75647e7652ffc471b8b05c52c9cb
Lepierre Alycia <alycia@MacBook-Pro-dAlycia.local>
2026-09-24 14:09:42 +0200

    Add gsap  animation

--- js/main.js
+++ js/main.js
@@ -6,38 +6,101 @@ gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPl
 /* Fonction-------------------*/
 
 /* Animation premier bloc ---------------*/ 
-gsap.from("#movingBlock1", {
-    x:"400px",
-    rotation: 360,
+gsap.from("#section1", {
+    x: "400px",
     duration: 2,
-    
 })
 
-/* Animation deuxième bloc- Scroll -----------------*/
-/*gsap.from("#movingBlock2",{ 
+/*Texte*/
+gsap.from(".section1-text",{
+    x:"-600px",
+    duration: 3,
+})
 
+
+gsap.from(".section1-txt",{
+    x:"600px",
+    duration: 3,
+})
+
+
+
+
+/* Src mozilla ----------*/
+/* Animation vêtement -------------------*/
+// 1.Sélection vêtements
+
+/*
+const tousLesVetements = document.querySelectorAll('.vetement');
+const section2 = document.getElementById('section2');
+
+// 2. La fonction qui fait tomber les vêtements
+function faireTomberVetements() {
+    tousLesVetements.forEach(function(vetement, index) {
+        setTimeout(function() {
+            vetement.classList.add('tombe');
+        }, index * 700); //écart entre chaque vêtement
+    });
+}
+
+// 3. "observateur" pour surveiller quand la Section 2 arrive à l'écran
+
+
+
+// src Mozilla //
+const observerOptions = {
+    root: null,  // Utilise la fenêtre du navigateur
+    threshold: 0.3  // Se déclenche quand 30% de la section 2 est visible à l'écran
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        // Si la section 2 est visible dans l'écran
+        if (entry.isIntersecting) {
+            faireTomberVetements(); // On lance l'animation !
+            observer.unobserve(entry.target); // On arrête de surveiller pour que l'animation ne se relance pas en boucle si on remonte la page
+        }
+    });
+}, observerOptions);
+
+// 4. On demande à l'observateur de surveiller notre section 2
+observer.observe(section2);
+
+
+*/
+
+
+
+
+
+/* Animation deuxième section -----------------*/
+gsap.fromTo(".vetement", 
+  { opacity: 0, y: 0 },
+  {
     scrollTrigger: {
-        trigger: "#section2",
-        start: "top 50%",
-        end: 'top 10%',
-        // markers: true,
-        id:'section2',
-        scrub: 1,
-        toggleActions: 'play none reverse reset',
+      trigger: "#section2",
+      start: "top 50%",
+      end: "top 10%",
+      scrub: 1,
     },
-    
-    
-    y: '50vh',
-    rotation: 1200,
-    duration: 2,
-})
+    opacity: 1,
+    y: 600,
+    duration: 1,
+    stagger: 1,
+  }
+);
 
 
 
+
+
+/* Animation troisième section ------------------------*/
+
+Draggable.create('.bloc')
 
 
 /* Animation troisième bloc - Timeline -----------------*/
-let tl = gsap.timeline({
+/*let tl = gsap.timeline({
     repeat: -1,
     yoyo: true,
 
@@ -54,6 +117,6 @@ tl.to('#movingBlock3',{x:100,})
 
 /* Animation quatrième bloc - Draggrable -----------------*/
 
-Draggable.create("#movingBlock4",{
+/*Draggable.create("#movingBlock4",{
     //type: "x",
-})
+})*/
